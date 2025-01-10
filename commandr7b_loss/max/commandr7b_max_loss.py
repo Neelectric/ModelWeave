2a8fa8d72d5a95f438e28a271df6bb067f791424
Neelectric <Neel.R@web.de>
2025-01-10 11:53:06 +0000

    Adding Command R7B mean/max/min heatmaps

---
+++ commandr7b_loss/max/commandr7b_max_loss.py
@@ -0,0 +1,166 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from datasets import load_dataset
+import matplotlib.pyplot as plt
+import imageio.v2 as imageio
+import os
+from tqdm import tqdm
+
+### LOAD MODEL, TOKENIZER, DATASET
+model_id = "CohereForAI/c4ai-command-r7b-12-2024"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="cuda:1",
+    torch_dtype=torch.bfloat16,
+    # attn_implementation="eager", # need to do this because "Olmo2Model is using Olmo2SdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, need to specify this"
+)
+dataset = load_dataset("lighteval/MATH", trust_remote_code=True)["test"]
+output_dir = "max_heatmaps"
+raw_heatmap_dir = os.path.join(output_dir, "raw_heatmaps")
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(raw_heatmap_dir, exist_ok=True)
+
+num_instances = len(dataset)
+# num_instances = 5
+global_min = float('inf')
+global_max = float('-inf')
+
+cross_dataset_maxs = []
+### PREPARE AND TOKENIZE FIRST PROMPT
+for instance_idx in tqdm(range(num_instances), dynamic_ncols=True):
+    instance = dataset[instance_idx]
+    problem = instance["problem"]
+    solution = instance["solution"]
+    chat = [
+        {"role": "user", "content": problem},
+    ]
+    templated = tokenizer.apply_chat_template(
+        chat, 
+        tokenize=False, 
+        add_generation_prompt=True)
+    inputs = tokenizer(templated, return_tensors="pt").to(model.device)
+    input_ids = inputs["input_ids"]
+
+    ### FORWARD PASS AND NEXT TOKEN PREDICTION
+    for param in model.parameters():
+        param.requires_grad = True
+    outputs = model(
+        **inputs,
+        labels=inputs["input_ids"],
+        return_dict_in_generate=True,
+        # output_attentions=True,
+    )
+    logits=outputs.logits[0],
+    next_token_logits = outputs.logits[:, -1, :].clone().float()
+    next_token_logits = next_token_logits.to(input_ids.device)
+
+    next_tokens = torch.argmax(next_token_logits, dim=-1)
+    input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
+    generation = tokenizer.batch_decode(input_ids)
+
+    ### CALCULATE LOSS AND BACKWARD PASS
+    target = input_ids[:, 1:].contiguous()
+    logits = outputs.logits[:, :-1, :].contiguous()
+    loss_fn = torch.nn.CrossEntropyLoss()
+    loss = loss_fn(outputs.logits.view(-1, model.config.vocab_size), target.view(-1))
+    tqdm.write(f"Loss: {loss.item()}")
+    loss.backward()
+
+    ### CALCULATE MEAN GRADIENT PER LAYER
+    instance_maxs = []
+    for layer_id, layer in enumerate(model.model.layers):
+        layer_maxs = []
+        # first we grab gradients from the 4 self_attn matrices
+        self_attn = layer.self_attn
+        self_attn_matrices = [self_attn.q_proj, self_attn.k_proj, self_attn.v_proj, self_attn.o_proj]
+        for matrix in self_attn_matrices:
+            grad = matrix.weight.grad
+            max_grad = grad.max()
+            layer_maxs.append(max_grad)
+        
+        # now try the same for 3 mlp matrices
+        mlp = layer.mlp
+        mlp_matrices = [mlp.gate_proj, mlp.up_proj, mlp.down_proj]
+        for matrix in mlp_matrices:
+            grad = matrix.weight.grad
+            max_grad = grad.max()
+            layer_maxs.append(max_grad)
+        instance_maxs.append(layer_maxs)
+    model.zero_grad()
+    # max, max and curtosis!
+    # could be interesting if gradient is skewed --> only updating certain neurons and others less
+    # plot matrices individually
+    # full ft is never low rank
+    # can we do lora? plot delta
+    # merge model wtih adapter then plot before and after
+    # maybe ideally gradient should update sparse set of neurons instead of everything
+    # maybe a loss function that enforces sparsity in weight updates?
+    # maybe we don't need sparsity but skewedness is enough
+    # get lora, then take delta W adn it may be more sparse and low rank than full finetuning
+
+
+    # separate math loss skewedness from correct/iincorrect predictions
+    # only do gradients if they exceed a certain threshold!
+    # research methods of skewedness, curtosis and Pearson mode skewness and Pearson median skewness
+    # check out skewedness of residual stream!
+    # gradients similar to saliency maps!
+    # this can be a preliminary study for some actual hardened mechinterp research
+
+
+    # take top 100 neurons where gradient is highest
+    # then knock them out
+    # what is impact? does accuracy go up or down
+    # if it goes up, then gradient was pinpointing most incorrect neurons
+    # feels like pruning? gradient based pruning?
+    # borrow ideas from lottery ticket hypothesis work?
+    # 3 datasets: math, entity tracking and natural lagnauge
+    # neurons that light up on entity tracking should 
+    # revisit monosemanticity and SAEs!
+
+    # Convert the list of lists to a tensor
+    instance_maxs_tensor = torch.tensor(instance_maxs).to(torch.float32) # need to convert to fp32 otherwise plt complains
+    cross_dataset_maxs.append(instance_maxs_tensor)
+
+    # Update global min and max values
+    global_min = min(global_min, instance_maxs_tensor.min().item())
+    global_max = max(global_max, instance_maxs_tensor.max().item())
+
+# # Plot the heatmap for each instance with consistent color scale
+# for instance_idx, instance_mins_tensor in tqdm(enumerate(cross_dataset_mins), dynamic_ncols=True):
+#     plt.figure(figsize=(12, 8))
+#     plt.title(f"Min of Gradients Heatmap of {model_id} on MATH - Instance {instance_idx + 1}")
+#     plt.imshow(instance_mins_tensor, cmap='viridis', aspect='auto', vmin=global_min, vmax=global_max)
+#     plt.colorbar(label='Min Gradient')
+#     plt.xlabel("Attention and MLP Matrices")
+#     plt.ylabel("Layers")
+#     plt.gca().invert_yaxis()  # Invert y-axis to start layers at 0 at the bottom
+#     plt.xticks(ticks=range(7), labels=['q_proj', 'k_proj', 'v_proj', 'o_proj', 'gate_proj', 'up_proj', 'down_proj'])
+#     plt.yticks(ticks=range(32), labels=range(32))
+#     plt.savefig(os.path.join(raw_heatmap_dir, f"heatmap_{instance_idx + 1}.png"))
+#     plt.close()
+
+# # Create a GIF from the saved heatmap images
+# images = []
+# for instance_idx in range(num_instances):
+#     image_path = os.path.join(raw_heatmap_dir, f"heatmap_{instance_idx + 1}.png")
+#     images.append(imageio.imread(image_path))
+
+# gif_path = os.path.join(output_dir, f"min_gradients_heatmaps_{num_instances}.gif")
+# imageio.mimsave(gif_path, images, duration=1)  # Increase duration to 2 seconds per frame
+
+# print(f"GIF saved at {gif_path}")
+
+cross_dataset_maxs_tensor = torch.stack(cross_dataset_maxs)
+averaged_dataset_maxs = cross_dataset_maxs_tensor.max(dim=0).values
+plt.figure(figsize=(12, 8))
+plt.title(f"Max Gradients Heatmap of {model_id} on MATH  - Average of {num_instances}")
+plt.imshow(averaged_dataset_maxs, cmap='viridis', aspect='auto')
+plt.colorbar(label='Max Gradient')
+plt.xlabel("Attention and MLP Matrices")
+plt.ylabel("Layers")
+plt.gca().invert_yaxis()  # Invert y-axis to start layers at 0 at the bottom
+plt.xticks(ticks=range(7), labels=['q_proj', 'k_proj', 'v_proj', 'o_proj', 'gate_proj', 'up_proj', 'down_proj'])
+plt.yticks(ticks=range(32), labels=range(32))
+plt.savefig(os.path.join(output_dir, f"max_of_{num_instances}_heatmap_.png"))
+plt.close()
--- finetune_olmo-2.py
+++ finetune_olmo-2.py
@@ -2,92 +2,157 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
-from trl import SFTTrainer, SFTConfig
+from trl import SFTTrainer, SFTConfig, DataCollatorForCompletionOnlyLM
 import wandb
 from wandb import AlertLevel
 
-wandb.init(
-    project="ModelWeave",
-    tags=["SFT, OLMO-2"],
-    )
-wandb.alert(
-    title="Starting MATH run",
-    text=f"We have started training",
-    level=AlertLevel.WARN,
-    wait_duration=300,
-)
+manual_seed = 42
+torch.random.manual_seed(manual_seed)
 
 
 ### LOAD MODEL, TOKENIZER AND DATASET
-model_id = "allenai/OLMo-2-1124-7B"
-instruct_model_id = "allenai/OLMo-2-1124-7B-Instruct"
+# model_id = "allenai/OLMo-2-1124-7B"
+model_id = "allenai/OLMo-2-1124-7B-Instruct"
+torch_dtype = torch.bfloat16
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     device_map="auto",
-    torch_dtype=torch.bfloat16, # we purposely load in bfloat16 for now to manage storage requirements
+    torch_dtype=torch_dtype, # we purposely load in bfloat16 for now to manage storage requirements
 )
-tokenizer = AutoTokenizer.from_pretrained(instruct_model_id)
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding=True)
-tokenizer.padding_side = "left"
-ds = load_dataset("lighteval/MATH", "all")
-print(ds)
-train = ds["train"]
-print(train)
-
-def template_func(example):
-    chat = [
-        {"role": "user", "content": example['problem']},
-        {"role": "assistant", "content": example['solution']}
-        ]
-    templated = tokenizer.apply_chat_template(chat, tokenize=True)
-    # print(templated)
-    return_dict = {
-        "input_ids": templated,
-        "attention_mask": torch.ones(len(templated), dtype=torch.bfloat16),
-        }
-    return return_dict
-
-templated_dataset = train.map(
-    template_func, 
-    # remove_columns=train.column_names,
-    # batched=True,
-    # num_proc=4,
+tokenizer = AutoTokenizer.from_pretrained(
+    model_id,
+    padding_side="left",
     )
+tokenizer.padding_side = "left"
+ds_name = "lighteval/MATH"
+ds = load_dataset(ds_name, "all")
+train = ds["train"]
 
-# train_dataset = tokenizer(templated_dataset, padding=True)
 
-# train_dataloader = DataLoader(
-#     train_dataset, 
-#     collate_fn=data_collator, 
-#     batch_size=16, 
-#     shuffle=False,
+def format_prompts(instance):
+    formatted_prompts = []
+    for i in range(len(instance["problem"])):
+        chat = [
+            {"role": "user", "content": instance['problem'][i]},
+            {"role": "assistant", "content": instance['solution'][i]}
+            ]
+        templated = tokenizer.apply_chat_template(chat, tokenize=False, )
+        # return_dict = {
+        #     "input_ids": templated,
+        #     "attention_mask": torch.ones(len(templated), dtype=torch.bfloat16),
+        #     }
+        formatted_prompts.append(templated)
+    return formatted_prompts
+
+# templated_dataset = train.map(
+#     template_func, 
 #     )
 
 
 
-### SET UP TRAINING ARGUMENTS
-training_args = SFTConfig(
-    output_dir="sfttrainer_outputdir",
-    report_to="wandb", # enables logging to W&B 😎
-    learning_rate=2e-4,
-    lr_scheduler_type="cosine",
-    num_train_epochs=1,
-    logging_steps=1,
-    
-    bf16=True, # use bfloat16 for training
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=1, # simulate larger batch sizes
+# def format_prompts(instance):
+#     formatted_prompts = []
+#     for i in range(len(instance["problem"])):
+#         problem = instance["problem"][i]
+#         raw_prompt = "Please solve the following problem: " + problem
+#         formatted_question = f"<|endoftext|><|user|>\n{raw_prompt} <|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+#         solution = instance["solution"][i] + EOS_TOKEN
+#         prompt = formatted_question + solution
+#         formatted_prompts.append(prompt)
+#     return formatted_prompts
 
+
+### TRAINING PREP
+# Core training variables and wandb init
+bf16 = (torch_dtype == torch.bfloat16)
+num_train_epochs = 1
+learning_rate = 2e-6
+# eval_save_steps = 1000
+eval_save_strategy = "epoch"
+batch_size = 1
+gradient_accumulation_steps = 1
+
+wandb_name = f"MATH_FT - {model_id} on {ds_name}"
+print(wandb_name)
+wandb.init(
+    name= wandb_name,
+    project="ModelWeave",
+    tags=[model_id],
+)
+# wandb.alert(
+#     title="Starting MATH run",
+#     text=f"We have started training",
+#     level=AlertLevel.WARN,
+#     wait_duration=300,
+# )
+
+training_args = SFTConfig(
+    # torch_dtype, VRAM and config setup
+    fp16= torch_dtype != torch.bfloat16,
+    bf16= torch_dtype == torch.bfloat16,
+
+    report_to="wandb",
+    # max_steps=max_steps,
+
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    gradient_checkpointing=False,
+    gradient_checkpointing_kwargs={"use_reentrant":False},
+
+    logging_steps=1,
+    max_seq_length=32768,
+    
+    dataset_num_proc=4,
+    dataloader_num_workers=4,
+    # dataloader_pin_memory=True,
+    # dataset_batch_size=batch_size,
+    # dataset_batch_size=2500,
+    # auto_find_batch_size=True,
+    # torch_compile=True,
+    # torch_compile_mode="max-autotune",
+
+    # LR and optimizer management
+    warmup_steps=0,
+    warmup_ratio=0.01,
+    learning_rate=learning_rate,
+    optim="adamw_torch_fused",
+    weight_decay=0,
+    lr_scheduler_type="linear",
+    # lr_scheduler_kwargs={"num_cycles": num_train_epochs},
+    seed=manual_seed,
+    data_seed=manual_seed,
+    max_grad_norm=1.0,
+
+    # Saving args
+    output_dir="sfttrainer_outputdir",
+    run_name= "MATH_FT - " + model_id + ds_name,
     push_to_hub=True,
     hub_model_id="OLMo-2-1124-7B_MATH",
     hub_private_repo=True,
+    save_total_limit=1,
+    # save_steps=eval_save_steps,
+    save_strategy=eval_save_strategy,
+    # load_best_model_at_end = True,
+
+    # Eval args
+    # eval_strategy=eval_save_strategy,
+    # eval_steps=eval_save_steps,
+    # per_device_eval_batch_size=batch_size,
+    # metric_for_best_model="eval_loss",
 )
+
+response_template = "\n<|assistant|>\n"
+response_template_tokenized = tokenizer.encode(response_template, add_special_tokens=False,)
 
 
 trainer = SFTTrainer(
     model,
-    train_dataset=templated_dataset,
+    tokenizer=tokenizer,
+    train_dataset=train,
     args=training_args,
+    formatting_func=format_prompts,
+    data_collator=DataCollatorForCompletionOnlyLM(tokenizer=tokenizer, response_template=response_template_tokenized),
     # data_collator=data_collator,
 )
 trainer.train()
